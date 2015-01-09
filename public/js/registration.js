4fe084fa55bca7b6d269f9aa1ff86679b47a45fb
Cody Casey <codyjamescasey@gmail.com>
2015-01-09 01:06:12 -0500

    react generated input fields! C= Though this is far from pretty...

--- public/js/registration.js
+++ public/js/registration.js
@@ -3,7 +3,6 @@
             return (
 
               <div className="registrationBox">
-              <h1>Registration</h1>
               <RegistrationForm />
               <RegistrationFormSubmitButton />
               </div>
@@ -16,8 +15,22 @@
           render: function() {
             return (
               <form className="registrationForm">
-              /*<RegistrationItem name = "First Name" type = "text">This is one RegistrationItem</RegistrationItem>
-              <RegistrationItem name = "Last Name" type = "text">This is another RegistrationItem</RegistrationItem>*/
+              <RegistrationItem value = "First Name" name = "first-name" type = "text" />
+              <RegistrationItem value = "Last Name" name = "last-name" type = "text" />
+              <RegistrationItem value = "Email Address" name = "email-address" type = "text" />
+              <RegistrationItem value = "School" name = "school" type = "text" />
+              <RegistrationItem value = "Shirt Size" name = "shirt-size" type = "radio" />
+              <RegistrationItem value = "Food Restrictions" name = "food-restrictions" type = "text" />
+              <RegistrationItem value = "First Hackathon?" name = "first-hackathon" type = "radio" />
+              <RegistrationItem value = "Why Owlhacks?" name = "why-owlhacks" type = "text" />
+              <RegistrationItem value = "Hardware Hack?" name = "hw-hack" type = "radio" />
+              <RegistrationItem value = "Teammate Names?" name = "teammate-names" type = "text" />
+              <RegistrationItem value = "Resume" name = "resume" type = "file" />
+              <RegistrationItem value = "Github Username" name = "github-username" type = "text" />
+              <RegistrationItem value = "LinkedIn Address" name = "linkedin-address" type = "text" />
+              <RegistrationItem value = "Twitter Handle" name = "twitter-handle" type = "text" />
+              <RegistrationItem value = "Personal Website" name = "personal-website" type = "text" />
+
               </form>
               );
           }
@@ -26,25 +39,19 @@
         var RegistrationFormSubmitButton = React.createClass({
           render: function() {
             return (
-              <div className="registrationFormSubmitButton">
-              Hello, I am a RegistrationFormSubmitButton.
-              </div>
+              <div className="registrationFormSubmitButton"></div>
               );
           }
         });
 
-/*
+
         var RegistrationItem = React.createClass({
-          render: function(){
-            if(this.props.type === "text"){
-              return(
-                <input class="form-input" placeholder= this.props.name type=this.props.type size="25" value="" required>
-                );
-            }
-            
+          render: function() {
+            return (<input type={this.props.type} placeholder={this.props.value} value="" />
+             );
           }
         });
-*/
+
         React.render(
           React.createElement(RegistrationBox, null),
           document.getElementById('content')
