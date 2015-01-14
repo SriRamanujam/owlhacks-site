d0a4e414e7034f216398c94f5103b746b3f43a53
Sri Ramanujam <sri.ramanujam@temple.edu>
2015-01-13 19:02:21 -0500

    Removed routes/subscribe.js because it did nothing and was getting in the way

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ var express = require('express'),
     path = require('path'),
     favicon = require('serve-favicon');
 
-var subscribe = require('./routes/subscribe');
-
 var app = express();
 
 app.set('port', process.env.PORT || 80);
 app.use(favicon(path.join(__dirname, 'public', 'img', 'favicon.ico')));
 app.use(express.static('public'));
+
+var routes = require('./routes')
 
 app.get('/', function(req, res) {
     res.status(200).sendFile(path.join(__dirname, 'public', 'dist', 'pages', 'index.html'));
