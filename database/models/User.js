f5d6b55eddcfcd61cdb586aac1d3d3fdf6f154c2
Sri Ramanujam <sri.ramanujam@temple.edu>
2015-01-14 23:47:26 -0500

    The beginnings of User model functionality

--- database/models/User.js
+++ database/models/User.js
@@ -5,16 +5,22 @@ var bookshelf = require('../db'),
     Shirt_Size = require('./Shirt_size'),
     Hack_Type = require('./Hack_type');
 
+var Promise = require('bluebird'),
+    bcrypt = Promise.promisifyAll(require('bcrypt'));
+
 var User = bookshelf.Model.extend({
     tableName: 'Users',
 
     update: function(new_fields) {
         //TODO
+        
     },
 
-    verifyPassword: function(password) {
+    verifyPassword: Promise.method(function(password) {
         //TODO
-    },
+        if (!password) throw new Error("Invalid password");
+        return bcrypt.compareAsync(this.get('password'), password); // does this work?
+    }),
 
     retrieve: function(fields, constraints) {
         //TODO
