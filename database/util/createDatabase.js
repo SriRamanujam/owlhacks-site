6d48b722e724ab8ba4c9274ca15414f59fe1e5ca
Sri Ramanujam <sri.ramanujam@temple.edu>
2015-01-16 22:04:58 -0500

    Learned how to use async.series, now createDatabase actually exits cleanly and functions as expected

--- database/util/createDatabase.js
+++ database/util/createDatabase.js
@@ -1,43 +1,47 @@
 var bookshelf = require('../db');
 var async = require('async');
 
-var createSchoolsTable = function() {
+var createSchoolsTable = function(cb) {
     bookshelf.knex.schema.createTable('Schools', function(t) {
         t.increments('id');
         t.text('school').notNullable().unique();
     }).then(function() {
         console.log("created schools table");
+        cb();
     });
 };
 
-var createStatusesTable = function() {
+var createStatusesTable = function(cb) {
     bookshelf.knex.schema.createTable('Statuses', function(t) {
         t.increments('id');
         t.text('status').notNullable().unique();
     }).then(function() {
         console.log("created statuses table");
+        cb();
     });
 };
 
-var createHackTypesTable = function() {
+var createHackTypesTable = function(cb) {
     bookshelf.knex.schema.createTable('Hack_Types', function(t) {
         t.increments('id');
         t.text('type').unique();
     }).then(function() {
         console.log("created hack types table");
+        cb();
     });
 };
 
-var createAllergiesTable = function() {
+var createAllergiesTable = function(cb) {
     bookshelf.knex.schema.createTable('Allergies', function(t) {
         t.increments('id');
         t.text('allergy').notNullable().unique();
     }).then(function() {
         console.log("created allergies table");
+        cb();
     });
 };
 
-var createUsersTable = function() {
+var createUsersTable = function(cb) {
     bookshelf.knex.schema.createTable('Users', function(t) {
         t.increments('id');
         t.text('first_name').notNullable();
@@ -54,51 +58,64 @@ var createUsersTable = function() {
         t.text('email').notNullable().unique();
     }).then(function() {
         console.log("created users table");
+        cb();
     });
 };
 
-var createAccountsTable = function() {
+var createAccountsTable = function(cb) {
     bookshelf.knex.schema.createTable('Accounts', function(t) {
         t.increments('id');
         t.integer('user_id').unsigned().references('id').inTable('Users');
         t.integer('status_id').unsigned().references('id').inTable('Statuses');
     }).then(function() {
         console.log("created accounts table");
+        cb();
     });
 };
 
-var createUserAllergiesTable = function() {
+var createUserAllergiesTable = function(cb) {
     bookshelf.knex.schema.createTable('User_Allergies', function(t) {
         t.increments('id');
         t.integer('user_id').unsigned().references('id').inTable('Users');
         t.integer('allergy_id').unsigned().references('id').inTable('Allergies');
     }).then(function() {
         console.log("created userallergies table");
+        cb();
     });
 };
 
 /**
  * The tables have to be created in the proper order, so that the db doesn't get
- * annoyed that tables are being referenced that don't exist yet.
+ * annoyed that tables are being referenced that don't exist yet. At the end, we
+ * also have to manually destroy the knex threadpool so that node can exit cleanly.
  */
-var createTables = function() {
-    createSchoolsTable();
-    createStatusesTable();
-    createHackTypesTable();
-    createAllergiesTable();
-    createUsersTable();
-    createAccountsTable();
-    createUserAllergiesTable();
+var doTheThing = function() {
+    async.series([
+        function(cb) {
+            createSchoolsTable(cb);
+        },
+        function(cb) {
+            createStatusesTable(cb);
+        },
+        function(cb) {
+            createHackTypesTable(cb);
+        },
+        function(cb) {
+            createAllergiesTable(cb);
+        },
+        function(cb) {
+            createUsersTable(cb);
+        },
+        function(cb) {
+            createAccountsTable(cb);
+        },
+        function(cb) {
+            createUserAllergiesTable(cb);
+        },
+        function(cb) {
+            bookshelf.knex.destroy(cb);
+        }
+    ]);
 };
-async.series([
-    function(cb) {
-        createTables();
-        cb();
-    },
-    function(cb) {
-        bookshelf.knex.destroy(cb);
-    }
-]);
-/*
-module.exports = createTables;
-*/
+
+doTheThing();
