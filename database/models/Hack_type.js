13aee21742ee415d81d2cb166df7c286fe0458b7
Sri Ramanujam <sri.ramanujam@temple.edu>
2015-01-17 10:20:13 -0500

    Updated table names to reflect reality

--- database/models/Hack_type.js
+++ database/models/Hack_type.js
@@ -2,7 +2,7 @@ var bookshelf = require('../db'),
     User = require('./User');
 
 var Hack_type = bookshelf.Model.extend({
-    tableName: 'Hack_types',
+    tableName: 'Hack_Types',
 
     users: function() {
         return this.hasMany(User);
--- database/models/Shirt_size.js
+++ database/models/Shirt_size.js
@@ -2,7 +2,7 @@ var bookshelf = require('../db'),
     User = require('./User');
 
 var shirt_size = bookshelf.Model.extend({
-    tableName: 'Shirt_sizes',
+    tableName: 'Shirt_Sizes',
 
     users: function() {
         return this.hasMany(User);
